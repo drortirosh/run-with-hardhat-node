b3cb6be3f632e6f6cf9133d74dd2d0099dda4823
Dror Tirosh <dror@opengsn.org>
2022-06-08 15:41:23 +0300

    extract and export "waitForCmdToStart"

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const { spawn, execSync } = require('child_process');
 
 /**
- * 
- * @param cmd external command to run.
- * @param args parameters for external command
- * @param waitFor wait for this output string of command, before launching innerCmd
- * @param innerCmd inner command to launch, once cmd had reached the "waitFor" output string
- * @param verbose true to dump cmd's output. false to hide extenral cmd output
- *              note that innerCmd's output is always dumped to output
+ * launch command, and wait for it to start.
+ * @param cmd
+ * @param args
+ * @param waitFor
+ * @param verbose
+ * @returns the started process.
  */
-async function runWithCmd({ cmd, args, waitFor, innerCmd, verbose }) {
-
+async function waitForCmdToStart({cmd, args, waitFor, verbose}) {
     let testrpc
-    return new Promise((resolve, reject) => {
+    if (typeof cmd != 'string') throw new Error('missing {cmd:"..."} param')
+    if (typeof waitFor != 'string') throw new Error('missing {waitFor:"..."} param')
+    if (args && !Array.isArray(args)) throw new Error('args param is not an array')
+
+    let lastData
+
+    await new Promise((resolve, reject) => {
         const handleError = (err) => {
             if(err.code === 'ENOENT')
                 return reject(new Error(`Could not find ${cmd}`))
@@ -28,9 +32,13 @@ async function runWithCmd({ cmd, args, waitFor, innerCmd, verbose }) {
         }
 
         testrpc.stdout.on('data', (data) => {
-            if (verbose) { process.stdout.write(data) }
+            const dataFiltered = data.toString().replace(/\d+/g,'')
+            if (verbose && dataFiltered != lastData) {
+                process.stdout.write(data)
+            }
+            lastData = dataFiltered
             if(data.includes(waitFor)) {
-                resolve()
+                resolve(testrpc)
             }
         })
 
@@ -46,11 +54,26 @@ async function runWithCmd({ cmd, args, waitFor, innerCmd, verbose }) {
         testrpc.on('close', (code) =>
             reject(new Error(`${cmd} exited early with code ${code}`))
         )
-    }).then(() => {
+    }) 
+}
+/**
+ * 
+ * @param cmd external command to run.
+ * @param args parameters for external command
+ * @param waitFor wait for this output string of command, before launching innerCmd
+ * @param innerCmd inner command to launch, once cmd had reached the "waitFor" output string
+ * @param verbose true to dump cmd's output. false to hide extenral cmd output
+ *              note that innerCmd's output is always dumped to output
+ */
+async function runWithCmd({ cmd, args, waitFor, innerCmd, verbose }) {
+
+    let testrpc
+    try {
+        testrpc = await waitForCmdToStart({cmd, args, waitFor, verbose})
         execSync(innerCmd, { stdio: 'inherit' })
-    }).finally(()=>{
+    } finally {
         if(testrpc) testrpc.kill()
-    })
+    }
 }
 
-module.exports = { runWithCmd }
+module.exports = { waitForCmdToStart, runWithCmd }
